8f5a48b4befc9f7dc358c50e93631cfc8bfbe69e
mfalbarifq1 <mfalbarif.q1@gmail.com>
2026-04-28 15:42:14 +0700

    program utama

--- friendFunctionSatu.cpp
+++ friendFunctionSatu.cpp
@@ -10,3 +10,14 @@ public:
 
 };
 
+void setNama(mahasiswa &a, string b)
+{
+    a.nama = b;
+    cout << a.nama;
+}
+
+int main(){
+    mahasiswa joko;
+    setNama(joko, "kairi kumar");
+    return 0;
+}
